ff6ad84f287c4a3a27b0bf2932c768c8fc5d3a69
Howard Ho <howard036060006@gmail.com>
2018-05-26 21:26:59 +0800

    add h3 forth

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def hello():
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}" \
            "<p>This is second build</p>" \
-           "<h3> Thrid Build !!! </h3>"
+           "<h3> Thrid Build !!! </h3>" \
+           "<h3> Forth Build !!! </h3>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
