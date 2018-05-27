152f2eefc64a7e648468cb82ec677f4aab46ac84
Howard Ho <howard036060006@gmail.com>
2018-05-27 23:53:54 +0800

    clean

--- app.py
+++ app.py
@@ -17,12 +17,8 @@ def hello():
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}" \
-           "<p>This is second build</p>" \
-           "<h3> Thrid Build !!! </h3>" \
-           "<h3> Forth Build !!! </h3>" \
-           "<h3> Five Build !!! </h3>" \
-           "<h3> Six build !!! </h3>"
+           "<b>Visits:</b> {visits}"
+
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
